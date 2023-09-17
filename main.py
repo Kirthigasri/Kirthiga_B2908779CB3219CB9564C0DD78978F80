4f48387b89d4e92ac23a02ed59362f39e1054f08
Kirthigasri <144258950+Kirthigasri@users.noreply.github.com>
2023-09-17 12:42:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-num=8
-num_sqrt=num**0.5
-print('The square root of %0.3f is %0.3f'%(num,num_sqrt))
+# Python program to create Bankaccount class
+# with both a deposit() and a withdraw() function
+class Bank_Account:
+    def __init__(self):
+        self.balance=0
+        print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
+ 
+    def deposit(self):
+        amount=float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+        print("\n Amount Deposited:",amount)
+ 
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("\n You Withdrew:", amount)
+        else:
+            print("\n Insufficient balance  ")
+ 
+    def display(self):
+        print("\n Net Available Balance=",self.balance)
+ 
+# Driver code
+  
+# creating an object of class
+s = Bank_Account()
+  
+# Calling functions with that class object
+s.deposit()
+s.withdraw()
+s.display()
